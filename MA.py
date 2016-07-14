199b2b06750779ba3bca8eb8356c5e980b0e5e0b
liqinglu <liqinglu2005@aliyun.com>
2016-07-14 14:04:30 +0800

    add MA

---
+++ MA.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
+import os,sys
+from stockutil import stockpool
+from stockutil import getStockData
+
+MAlist = [5,10,30,60]
+MAcolor = ['w','y','m','c']
+
+for stockfile in stockpool:
+    basen = os.path.splitext(stockfile)[0]
+    data = getStockData(stockfile)
+    if data is None:
+        print '%s is missing'%stockfile
+        continue
+
+    dataMA = data.set_index('date')
+    dataMA = dataMA.resample('B').fillna(method='ffill')
+    #only see MA data in recent one year
+    dataMA.ending[-360:].plot(color='b')
+    for days,usedcolor in zip(MAlist,MAcolor):
+        pd.rolling_mean(dataMA.ending[-360:],days).plot(color=usedcolor)
+
+    filename = basen + '_' + "MA_5_10_30_60.png"
+    #savefig(filename)
+    plt.show()
+    data,dataMA = None,None
